181160e83f2963902aebb198ad9a59a013689eb0
wasim-basha-m <wasimbasha1865@gmail.com>
2024-02-26 06:38:40 +0530

    New changes

--- Java/AccordLab/src/j12l1/LabOne.java
+++ Java/AccordLab/src/j12l1/LabOne.java
@@ -3,20 +3,40 @@ package j12l1;
 import java.util.Scanner;
 
 public class LabOne {
-
 	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of an array : ");
-		int a=sc.nextInt();
-		System.out.println("Enter the values : ");
-		int c=0;
-		for(int i=0;i<a;i++) {
-			c=sc.nextInt();
-			System.out.println(c);
-		}
-		
-		System.out.println(c);
-	}
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("Enter the size of the array: ");
+		int size = scanner.nextInt();
+		int[] array = new int[size];
+		System.out.println("Enter the elements of the array:");
+		for (int i = 0; i < size; i++) {
+			System.out.print("Enter element at position " + i + ": ");
+			array[i] = scanner.nextInt();
+		}
+
+		try {
+
+			System.out.print("Enter the position of the dividend: ");
+			int dividendPosition = scanner.nextInt();
+
+			System.out.print("Enter the position of the divisor: ");
+			int divisorPosition = scanner.nextInt();
+			int dividend = array[dividendPosition];
+			int divisor = array[divisorPosition];
+			int quotient = dividend / divisor;
+			int remainder = dividend % divisor;
+
+			System.out.println("Quotient: " + quotient);
+			System.out.println("Remainder: " + remainder);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array index out of bounds. Please enter valid positions.");
+		} catch (ArithmeticException e) {
+			System.out.println("Division by zero error. Please make sure the divisor is not zero.");
+		} catch (Exception e) {
+			System.out.println("An error occurred: " + e.getMessage());
+		} finally {
+			scanner.close();
+		}
+	}
 }
